cfcdef60d2a63e3fc32197a11fe74fefec243890
nithya <nithukitty112@gmail.com>
2025-04-21 15:30:16 +0530

    commit

---
+++ react-MovieSearchApp/src/App.jsx
@@ -0,0 +1,23 @@
+// import Header from './components/Header';
+// import Footer from './components/Footer';
+
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import MovieDetails from './pages/MovieDetails';
+
+function App() {
+  return (
+    <>
+      {/* <Header /> */}
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movie/:id" element={<MovieDetails />} />
+        </Routes>
+      </main>
+      {/* <Footer /> */}
+    </>
+  );
+}
+
+export default App;
